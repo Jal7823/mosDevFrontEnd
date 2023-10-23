4b7c0b870f23c3a118de9a86b2ea0d5d0f453847
jal7823 <Jal7823@gmail.com>
2023-10-23 16:19:23 -0300

    fix(Cards): :hammer: fix display flex and display responsive
    fix display flex and display responsive

--- src/components/SecondNavbar.jsx
+++ src/components/SecondNavbar.jsx
@@ -1,14 +1,36 @@
 import {NavLink} from 'react-router-dom'
+import fetchData from '../utils/crud'
+import { useEffect, useState } from 'react'
+
 
 function SecondNavbar() {
+
+  const [Categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchDataCategories = async () => {
+      try {
+        const data = await fetchData('post/categorie/');
+        setCategories(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDataCategories();
+  }, []);
+
+  console.log(Categories);
+  
+
   return (
     <>
-        <div className="bg-bg-customyellow">
-            <ul className="flex justify-center fw-bold p-4">
-                <li className="ml-2"><NavLink to="">Tech</NavLink></li>
-                <li className="ml-2"><NavLink to="">enlace</NavLink></li>
-                <li className="ml-2"><NavLink to="">enlace</NavLink></li>
-                <li className="ml-2"><NavLink to="">enlace</NavLink></li>
+        <div className="dark:bg-bg-custumgray dark:text-white text-center border border-gray-200 rounded-lg shadow m-2 hidden md:block">
+            <ul className="flex flex-col justify-center fw-bold p-4">
+                <li className="ml-2 w-100%"><NavLink to="">Tech</NavLink></li>
+                <li className="ml-2 w-100%"><NavLink to="">enlace</NavLink></li>
+                <li className="ml-2 w-100%"><NavLink to="">enlace</NavLink></li>
+                <li className="ml-2 w-100%"><NavLink to="">enlace</NavLink></li>
             </ul>
         </div>
     </>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,15 +6,13 @@ import fetchData from "../utils/crud";
 
 //components
 import Title from "../components/Title";
-import Sliders from '../components/Sliders'
+import Sliders from "../components/Sliders";
+import SecondNavbar from "../components/SecondNavbar";
 
 //pages
 
-
-
 function Index() {
   const [posts, setPosts] = useState([]);
-  
 
   useEffect(() => {
     const getData = async () => {
@@ -34,20 +32,27 @@ function Index() {
       <Sliders />
       <Title text="Ultimos Post" />
       <hr className="w-9/12 mx-auto" />
-      <div className="flex flex-wrap justify-around dark:bg-bg-custumgray mt-4 p-6">
-        {posts.map((el) => (
-          <div key={el.id} className="dark:bg-bg-customgray  ">
-            <Cards
-              image={el.principal_image}
-              id={el.id}
-              title={el.title}
-              content={el.content}
-              author={el.author}
-              created_at={el.created_at}
-              link='https://www.google.com'
-            />
-          </div>
-        ))}
+
+      <div className="flex flex-wrap dark:bg-bg-custumgray">
+        <div className="flex flex-wrap justify-around dark:bg-bg-custumgray mt-4 p-6 w-5/6">
+          {posts.map((el) => (
+            <div key={el.id} className="dark:bg-bg-customgray  ">
+              <Cards
+                image={el.principal_image}
+                id={el.id}
+                title={el.title}
+                content={el.content}
+                author={el.author}
+                created_at={el.created_at}
+                link="https://www.google.com"
+              />
+            </div>
+          ))}
+        </div>
+        <div className=" w-1/6">
+          <SecondNavbar />
+        </div>
+
       </div>
     </div>
   );
