128935a3c7ca536182b89564e696544fe64074be
jal7823 <jal7823@gmail.com>
2023-11-27 09:58:03 -0300

    fix: :hammer: Index title
    Index title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@
 function Title({text}) {
   return (
     <>
-        <h3 className="text-4xl text-center font-bold mt-4 mb-4 dark:text-white p-4">{text}</h3>
+        <h3 className="text-4xl text-center font-bold mt-4  dark:bg-bg-custom-gray dark:text-white p-4">{text}</h3>
     </>
   )
 }
