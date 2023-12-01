06a8f568aabb4ed37a3eee72cc379192f723ee32
jal7823 <jal7823@gmail.com>
2023-12-01 09:21:53 -0300

    fix: :hammer: deploy
    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://Jal7823.github.io/mosDevFrontEnd/'
+  base:'https://Jal7823.github.io/mosDevFrontEnd'
 })
